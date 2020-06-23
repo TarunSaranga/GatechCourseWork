4cd2e2748c895a4cbe5bb50d9b08139d5059b3ba
Tarun Saranga <tarun.saranga116@gmail.com>
2020-06-23 12:05:00 -0700

    Add files via upload

---
+++ AI4R/ProblemSet4/Q5.py
@@ -0,0 +1,115 @@
+# --------------
+# USER INSTRUCTIONS
+#
+# Write a function called stochastic_value that 
+# returns two grids. The first grid, value, should 
+# contain the computed value of each cell as shown 
+# in the video. The second grid, policy, should 
+# contain the optimum policy for each cell.
+#
+# --------------
+# GRADING NOTES
+#
+# We will be calling your stochastic_value function
+# with several different grids and different values
+# of success_prob, collision_cost, and cost_step.
+# In order to be marked correct, your function must
+# RETURN (it does not have to print) two grids,
+# value and policy.
+#
+# When grading your value grid, we will compare the
+# value of each cell with the true value according
+# to this model. If your answer for each cell
+# is sufficiently close to the correct answer
+# (within 0.001), you will be marked as correct.
+
+delta = [[-1, 0 ], # go up
+         [ 0, -1], # go left
+         [ 1, 0 ], # go down
+         [ 0, 1 ]] # go right
+
+delta_name = ['^', '<', 'v', '>'] # Use these when creating your policy grid.
+
+# ---------------------------------------------
+#  Modify the function stochastic_value below
+# ---------------------------------------------
+
+def stochastic_value(grid,goal,cost_step,collision_cost,success_prob):
+    failure_prob = (1.0 - success_prob)/2.0 # Probability(stepping left) = prob(stepping right) = failure_prob
+    value = [[collision_cost for col in range(len(grid[0]))] for row in range(len(grid))]
+    policy = [[' ' for col in range(len(grid[0]))] for row in range(len(grid))]
+    
+    change = True
+
+    def causes_collision(x,y):
+        return x < 0 or x == len(grid) or y < 0 or y == len(grid[0]) or grid[x][y] == 1
+
+    while change:
+        change = False
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if goal[0] == x and goal[1] == y:
+                    if value[x][y] > 0:
+                        value[x][y] = 0
+
+                        change = True
+
+                elif grid[x][y] == 0:
+                    for a in range(len(delta)):
+                        x2 = x + delta[a][0]
+                        y2 = y + delta[a][1]
+
+                        if causes_collision(x2, y2):
+                            continue
+                        else:
+                            xl = x + delta[(a + 1) % len(delta)][0]
+                            yl = y + delta[(a + 1) % len(delta)][1]
+                            xr = x + delta[(a - 1) % len(delta)][0]
+                            yr = y + delta[(a - 1) % len(delta)][1]
+                            f_cost = value[x2][y2] * success_prob
+                            l_cost = (collision_cost if causes_collision(xl,yl) else value[xl][yl]) * failure_prob
+                            r_cost = (collision_cost if causes_collision(xr,yr) else value[xr][yr]) * failure_prob
+                            v2 = f_cost + l_cost + r_cost + cost_step
+
+                            if v2 < value[x][y]:
+                                change = True
+                                value[x][y] = v2
+                                policy[x][y] = delta_name[a]
+
+    policy[goal[0]][goal[1]] = '*'
+    
+    return value, policy
+
+# ---------------------------------------------
+#  Use the code below to test your solution
+# ---------------------------------------------
+
+grid = [[0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 1, 0]]
+goal = [0, len(grid[0])-1] # Goal is in top right corner
+cost_step = 1
+collision_cost = 1000
+success_prob = 0.5
+
+value,policy = stochastic_value(grid,goal,cost_step,collision_cost,success_prob)
+for row in value:
+    print row
+for row in policy:
+    print row
+
+# Expected outputs:
+#
+#[471.9397246855924, 274.85364957758316, 161.5599867065471, 0],
+#[334.05159958720344, 230.9574434590965, 183.69314862430264, 176.69517762501977], 
+#[398.3517867450282, 277.5898270101976, 246.09263437756917, 335.3944132514738], 
+#[700.1758933725141, 1000, 1000, 668.697206625737]
+
+
+#
+# ['>', 'v', 'v', '*']
+# ['>', '>', '^', '<']
+# ['>', '^', '^', '<']
+# ['^', ' ', ' ', '^']
