17d21bb9d3f590fe6f95e468495d7184c6b09636
Tarun Saranga <tarun.saranga116@gmail.com>
2020-06-23 19:05:54 -0700

    Add files via upload

---
+++ ML4T/marketsim/marketsim_test.py
@@ -0,0 +1,135 @@
+"""MC2-P1: Market simulator.  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			  	 		  		  		    	 		 		   		 		  
+Atlanta, Georgia 30332  		   	  			  	 		  		  		    	 		 		   		 		  
+All Rights Reserved  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Template code for CS 4646/7646  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			  	 		  		  		    	 		 		   		 		  
+works, including solutions to the projects assigned in this course. Students  		   	  			  	 		  		  		    	 		 		   		 		  
+and other users of this template code are advised not to share it with others  		   	  			  	 		  		  		    	 		 		   		 		  
+or to make it available on publicly viewable websites including repositories  		   	  			  	 		  		  		    	 		 		   		 		  
+such as github and gitlab.  This copyright statement should not be removed  		   	  			  	 		  		  		    	 		 		   		 		  
+or edited.  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+We do grant permission to share solutions privately with non-students such  		   	  			  	 		  		  		    	 		 		   		 		  
+as potential employers. However, sharing with other current or future  		   	  			  	 		  		  		    	 		 		   		 		  
+students of CS 7646 is prohibited and subject to being investigated as a  		   	  			  	 		  		  		    	 		 		   		 		  
+GT honor code violation.  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+-----do not edit anything above this line---  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Student Name: Tharun Saranga
+GT User ID: tsaranga3
+GT ID: 903
+"""  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+import pandas as pd  		   	  			  	 		  		  		    	 		 		   		 		  
+import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
+import datetime as dt  		   	  			  	 		  		  		    	 		 		   		 		  
+import os  		   	  			  	 		  		  		    	 		 		   		 		  
+from util import get_data, plot_data  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000, commission=9.95, impact=0.005):  		   	  			  	 		  		  		    	 		 		   		 		  
+    # this is the function the autograder will call to test your code  		   	  			  	 		  		  		    	 		 		   		 		  
+    # NOTE: orders_file may be a string, or it may be a file object. Your  		   	  			  	 		  		  		    	 		 		   		 		  
+    # code should work correctly with either input  		   	  			  	 		  		  		    	 		 		   		 		  
+    # TODO: Your code here  		   	  			  	 		  		  		    	 		 		   		 		  
+    orders_df = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan'])	   	  			  	 		  		  		    	 		 		   		 		  
+    orders_df.sort_index(inplace=True)
+    #print(orders_df.head())
+    sym = list(set(orders_df['Symbol'].values))
+    #print(sym)
+    start_date = orders_df.index.values[0]
+    end_date = orders_df.index.values[-1]
+    date_range = pd.date_range(start_date, end_date)
+    
+    prices = get_data(sym,date_range)
+    #print(prices.head())
+    prices['Cash'] = np.ones(prices.shape[0])
+    #print(prices.head())
+    shares = prices*0.0
+    shares.iloc[0,-1] = start_val
+    
+    order = orders_df.iloc[0]
+    
+    for idx2, row2 in orders_df.iterrows():
+        order_price = prices[row2[0]].loc[idx2]
+        order_units = row2[2]
+        
+        if row2[1] == "BUY":
+            sign = -1
+        else:
+            sign = 1
+        
+        shares.loc[idx2,row2[0]] += order_units*sign*-1
+        shares.loc[idx2,"Cash"] += order_units*order_price*sign
+        shares.loc[idx2,"Cash"] -= commission
+        share_impact = order_units*order_price*impact
+        shares.loc[idx2,"Cash"] -= share_impact
+        
+    for i in range(1,shares.shape[0]):
+        for j in range(0,shares.shape[1]):
+            shares.iloc[i,j] += shares.iloc[i-1,j]
+    
+    portvals = prices * shares
+    portvals["portval"] = portvals.sum(axis=1)
+    portvals["daily_returns"] = portvals["portval"][1:]/portvals["portval"][:-1].values - 1
+    portvals["daily_returns"][0] = 0
+    portvals = portvals.iloc[:,-2:-1]
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    #return rv  		   	  			  	 		  		  		    	 		 		   		 		  
+    return portvals  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+def author():
+    return 'tsaranga3'
+
+
+def test_code():  		   	  			  	 		  		  		    	 		 		   		 		  
+    # this is a helper function you can use to test your code  		   	  			  	 		  		  		    	 		 		   		 		  
+    # note that during autograding his function will not be called.  		   	  			  	 		  		  		    	 		 		   		 		  
+    # Define input parameters  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    of = "./orders/orders-02.csv"  		   	  			  	 		  		  		    	 		 		   		 		  
+    sv = 1000000  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    # Process orders  		   	  			  	 		  		  		    	 		 		   		 		  
+    portvals = compute_portvals(orders_file = of, start_val = sv)  		   	  			  	 		  		  		    	 		 		   		 		  
+    if isinstance(portvals, pd.DataFrame):  		   	  			  	 		  		  		    	 		 		   		 		  
+        portvals = portvals[portvals.columns[0]] # just get the first column  		   	  			  	 		  		  		    	 		 		   		 		  
+    else:  		   	  			  	 		  		  		    	 		 		   		 		  
+        "warning, code did not return a DataFrame"  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    # Get portfolio stats  		   	  			  	 		  		  		    	 		 		   		 		  
+    # Here we just fake the data. you should use your code from previous assignments.  		   	  			  	 		  		  		    	 		 		   		 		  
+    start_date = dt.datetime(2008,1,1)  		   	  			  	 		  		  		    	 		 		   		 		  
+    end_date = dt.datetime(2008,6,1)  
+    rfr=0
+    sf=252
+    cum_ret=(portvals[-1]/portvals[0])-1
+    dr=(portvals/portvals.shift(1))-1
+    avg_daily_ret=dr.mean()
+    std_daily_ret=dr.std()
+    a=np.sqrt(sf)
+    sharpe_ratio=a*((dr.subtract(rfr)).mean())/std_daily_ret		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    # Compare portfolio against $SPX  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Date Range: {start_date} to {end_date}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print()  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Sharpe Ratio of Fund: {sharpe_ratio}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    #print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print()  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Cumulative Return of Fund: {cum_ret}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    #print(f"Cumulative Return of SPY : {cum_ret_SPY}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print()  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Standard Deviation of Fund: {std_daily_ret}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    #print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print()  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Average Daily Return of Fund: {avg_daily_ret}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    #print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")  		   	  			  	 		  		  		    	 		 		   		 		  
+    print()  		   	  			  	 		  		  		    	 		 		   		 		  
+    print(f"Final Portfolio Value: {portvals[-1]}")  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+if __name__ == "__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
+    test_code()  		   	  			  	 		  		  		    	 		 		   		 		  
